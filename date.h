3236504e6a3eea3e3c67cbe80ed63d16fa3b4b64
Boros <mboros@wrenvironmental.com>
2017-04-28 14:51:08 -0400

    finished Date declarations

--- date.h
+++ date.h
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <string>
 #include <iostream>
+#include <stdexcept>
+#include <limits>
 
 namespace Chrono {
 	enum class Month {
@@ -13,9 +15,25 @@ namespace Chrono {
 		int d;
 		Day(int dd):
 			d(dd) {
-				if (dd < 0 || dd > 31)
-					std::cout << "Invalid day";
+				if (dd < 1 || dd > 31)
+					throw invalid_argument("Day out of range");
 			}
 	};
+	
+	class Date {
+	public:
+		Date(int y, Month m, Day d);
+		Day day() const { return d; }
+		Month month() const { return m; }
+		int year() const { return y; }
+		
+		void add_day(int n);
+		void add_month(int n);
+		void add_year(int n);
 
+	private:
+		int y;
+		Month m;
+		Day d;
+	};
 }
