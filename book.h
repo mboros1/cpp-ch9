3c6a1d0cdc5ace004730b58f39a07a25d5870818
Boros <mboros@wrenvironmental.com>
2017-05-02 14:54:24 -0400

    added constructor for Book

--- book.h
+++ book.h
@@ -7,22 +7,28 @@ namespace std {
 	namespace Book {
 		class Book {
 		public:
-			string isbn() { return ISBN; }
-			string title() { return Title; }
-			string author() { return Author; }
+			std::string isbn() { return ISBN; }
+			std::string title() { return Title; }
+			std::string author() { return Author; }
 			Chrono::Date copyright() { return Copyright; }
 			bool is_out() { return Is_Out; }
 			void check_in() { Is_Out = false; }
 			void check_out() { Is_Out = true; }
+			Book();
+			Book(std::string isbn, std::string title, std::string author, Chrono::Date copyright);
 
 		private:
-			string ISBN;
-			string Title;
-			string Author;
+			std::string ISBN;
+			std::string Title;
+			std::string Author;
 			Chrono::Date Copyright;
 			bool Is_Out;
 		};
 
+		Book::Book(std::string isbn, std::string title, std::string author, Chrono::Date copyright)
+			:ISBN(isbn), Title(title), Author(author), Copyright(copyright), Is_Out(false)
+		{
 
+		}
 	}
 }
